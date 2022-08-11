41438d809c822e8992fcf431d82e30f0bb90658d
ajtvince <41200248+ajtvince@users.noreply.github.com>
2022-08-11 13:43:47 -0400

    changes to homepage design/flow

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,29 +1,98 @@
 import { useEffect, useState } from 'react';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCode, faDatabase, faPalette, faPenRuler, faTerminal, faUpRightAndDownLeftFromCenter } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRight, faCode, faDatabase, faPalette, faPenRuler, faTerminal, faUpRightAndDownLeftFromCenter } from '@fortawesome/free-solid-svg-icons';
 import { faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons';
 import "./Home.css";
 function Home() {
 
+  const showSkills = () => {
+    let homeInfo = document.getElementsByClassName('homeInfo')[0];
+    let animType = [
+      { opacity: 1, offset: 0 },
+      { opacity: 0, offset: 1 },
+    ];
+    let animTime = {
+        duration: 400,
+        fill: 'forwards',
+    }
+    homeInfo.animate(animType, animTime);
+    document.getElementsByClassName('homeSkillsContainer')[0].style.display = 'block';
+    setTimeout(function(){
+      homeInfo.className = 'hideHomeInfo';
+      let homeSkills = document.getElementsByClassName('homeSkills')[0];
+      let animType2 = [
+        { opacity: 0, offset: 0 },
+        { opacity: 1, offset: 1 },
+      ];
+      homeSkills.animate(animType2, animTime);
+      for (let x=0; x<9; x++) {
+        let currentSkill = homeSkills.getElementsByClassName('skillContainer')[x];
+        let tempAnim = [
+          { opacity: 0, offset: 0 },
+          { opacity: 1, offset: 1 },
+        ];
+        let tempTimeout = 1000 * (x/10)
+        setTimeout(function(){
+          currentSkill.animate(tempAnim, animTime);
+        }, tempTimeout);
+      }
+      setTimeout(function(){
+        for (let x=0; x<8; x++) {
+          let currentSkill = homeSkills.getElementsByClassName('skillContainer')[x].getElementsByTagName('div')[1];
+          let tempAnim = [
+            { width: '0px', offset: 0 },
+            { width: '70%', offset: 1 },
+          ];
+          if (x >= 3) {
+            tempAnim = [
+              { width: '0px', offset: 0 },
+              { width: '60%', offset: 1 },
+            ];
+          }
+          if (x >= 6) {
+            tempAnim = [
+              { width: '0px', offset: 0 },
+              { width: '40%', offset: 1 },
+            ];
+          }
+          let tempTimeout = 800 * (x/20)
+          setTimeout(function(){
+            currentSkill.animate(tempAnim, animTime);
+          }, tempTimeout);
+        }
+      }, 400);
+    }, 400);
+  }
 
+  useEffect(() => {
+    console.log('ran at beginning');
+    document.getElementsByClassName('homeSkillsContainer')[0].style.display = 'none';
+  }, []);
 
   return(
     <div className='homeContainer'>
       <div className='homeOverview'>
-        <div>Andrew Vince</div>
-        <div>Front End Developer</div>
-        <img src='./images/hike.jpg'></img>
-        <div className='homeSkills'>
-          <div>Top Skills</div>
-          <div className='skillContainer'><FontAwesomeIcon id='skillIcon' icon={faJsSquare} /><span>JavaScript</span></div>
-          <div className='skillContainer'><FontAwesomeIcon id='skillIcon' icon={faReact} /><span>React</span></div>
-          <div className='skillContainer'><FontAwesomeIcon id='skillIcon' icon={faCode} /><span>HTML</span></div>
-          <div className='skillContainer'><FontAwesomeIcon id='skillIcon' icon={faPenRuler} /><span>CSS</span></div>
-          <div className='skillContainer'><FontAwesomeIcon id='skillIcon' icon={faTerminal} /><span>PowerShell</span></div>
-          <div className='skillContainer'><FontAwesomeIcon id='skillIcon' icon={faDatabase} /><span>SQL</span></div>
-          <div className='skillContainer'><FontAwesomeIcon id='skillIcon' icon={faUpRightAndDownLeftFromCenter} /><span>APIs</span></div>
-          <div className='skillContainer'><FontAwesomeIcon id='skillIcon' icon={faPalette} /><span>Design</span></div>
+        <div className='homeInfo'>
+          <div>Andrew Vince</div>
+          <div>Front End Developer</div>
+          <img src='./images/hike.jpg'></img>
+          <button onClick={showSkills}>Skills <FontAwesomeIcon id='skillIcon' icon={faArrowRight} /></button>
+        </div>
+        <div className='homeSkillsContainer'>
+          <div className='homeSkills'>
+            <div>Top Skills</div>
+            <div className='skillContainer'><div><FontAwesomeIcon id='skillIcon' icon={faJsSquare} /><span>JavaScript</span></div><div id='skillProgress'>Advanced</div></div>
+            <div className='skillContainer'><div><FontAwesomeIcon id='skillIcon' icon={faCode} /><span>HTML</span></div><div id='skillProgress'>Advanced</div></div>
+            <div className='skillContainer'><div><FontAwesomeIcon id='skillIcon' icon={faPenRuler} /><span>CSS</span></div><div id='skillProgress'>Advanced</div></div>
+            <div className='skillContainer'><div><FontAwesomeIcon id='skillIcon' icon={faReact} /><span>React</span></div><div id='skillProgress'>Intermediate</div></div>
+            <div className='skillContainer'><div><FontAwesomeIcon id='skillIcon' icon={faPalette} /><span>Design</span></div><div id='skillProgress'>Intermediate</div></div>
+            <div className='skillContainer'><div><FontAwesomeIcon id='skillIcon' icon={faTerminal} /><span>PowerShell</span></div><div id='skillProgress'>Intermediate</div></div>
+            <div className='skillContainer'><div><FontAwesomeIcon id='skillIcon' icon={faDatabase} /><span>SQL</span></div><div id='skillProgress'>Beginner</div></div>
+            <div className='skillContainer'><div><FontAwesomeIcon id='skillIcon' icon={faUpRightAndDownLeftFromCenter} /><span>APIs</span></div><div id='skillProgress'>Beginner</div></div>
+            <div className='skillContainer'><Link to='/projects'><button id='toProjectsButton'>Projects  <FontAwesomeIcon id='skillIcon' icon={faArrowRight} /></button></Link></div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -20,18 +20,6 @@ function Projects() {
         <div id='projectDesc'>This was my first experience working on a professional website for a company. Although I was towards the beginning of my degree when I started and finished this project, it was still a huge learning experience for me in regards to building a complete static website and working with a marketing team. I used HTML, CSS, JavaScript, and PHP on this project alongside JavaScript libraries when additional functionality is needed.</div>
       </div>
       <div className='projectContainer'>
-        <div id='projectName'>Online Store (MERN)&nbsp;&nbsp;&nbsp;<a target='_blank' href='https://github.com/ajtvince/senior-project'><FontAwesomeIcon id='skillIcon' icon={faGithub} /></a></div>
-        <div id='projectSkills'>
-          <FontAwesomeIcon id='skillIcon' icon={faReact} />
-          <FontAwesomeIcon id='skillIcon' icon={faNodeJs} />
-          <FontAwesomeIcon id='skillIcon' icon={faDatabase} />
-          <FontAwesomeIcon id='skillIcon' icon={faHtml5} />
-          <FontAwesomeIcon id='skillIcon' icon={faCss3Alt} />
-        </div>
-        <img id='projectImg' src='./images/inthome.png' alt='img'></img>
-        <div id='projectDesc'>This was a school project I created to learn more about full-stack development using the MERN (MongoDB, Express.js, ReactJS, Node.js) Stack. This project was focused mainly on the back-end and connecting that to databases (MongoDB), sending and retrieving data, and connecting the back-end and front-end together. Although this project does not include any type of encryption or data validation aside from a very basic login system, it was a great introduction to back-end development for me.</div>
-      </div>
-      <div className='projectContainer'>
         <div id='projectName'>Custom Audio Player&nbsp;&nbsp;&nbsp;<a target='_blank' href='https://github.com/ajtvince/projects'><FontAwesomeIcon id='skillIcon' icon={faGithub} /></a></div>
         <div id='projectSkills'>
           <FontAwesomeIcon id='skillIcon' icon={faReact} />
@@ -50,6 +38,18 @@ function Projects() {
         </div>
         <img id='projectImg' src='./images/whotoban2.png' alt='img'></img>
         <div id='projectDesc'>League of Legends application telling players who to ban based off of who they win or lose against the most. Learned about using APIs, state, data management, and more.</div>
+      </div>
+      <div className='projectContainer'>
+        <div id='projectName'>Online Store (MERN)&nbsp;&nbsp;&nbsp;<a target='_blank' href='https://github.com/ajtvince/senior-project'><FontAwesomeIcon id='skillIcon' icon={faGithub} /></a></div>
+        <div id='projectSkills'>
+          <FontAwesomeIcon id='skillIcon' icon={faReact} />
+          <FontAwesomeIcon id='skillIcon' icon={faNodeJs} />
+          <FontAwesomeIcon id='skillIcon' icon={faDatabase} />
+          <FontAwesomeIcon id='skillIcon' icon={faHtml5} />
+          <FontAwesomeIcon id='skillIcon' icon={faCss3Alt} />
+        </div>
+        <img id='projectImg' src='./images/inthome.png' alt='img'></img>
+        <div id='projectDesc'>This was a school project I created to learn more about full-stack development using the MERN (MongoDB, Express.js, ReactJS, Node.js) Stack. This project was focused mainly on the back-end and connecting that to databases (MongoDB), sending and retrieving data, and connecting the back-end and front-end together. Although this project does not include any type of encryption or data validation aside from a very basic login system, it was a great introduction to back-end development for me.</div>
       </div>
       <div className='projectContainer'>
         <div id='projectName'>TFT Buddy&nbsp;&nbsp;&nbsp;<a target='_blank' href='https://github.com/ajtvince/tftbuddy'><FontAwesomeIcon id='skillIcon' icon={faGithub} /></a></div>
